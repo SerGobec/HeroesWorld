686352a2b2385e59e75298f9e0f263944ac15009
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-10-05 00:59:49 +0300

    init layers (clear architecture)

---
+++ backend/HeroesWorld/HeroesWorld.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesWorld.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public long Id;
+    }
+}
